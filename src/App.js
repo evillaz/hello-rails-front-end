1c74f1a39705b54cf327d6edf582ddc9a26c901c
evillaz <evillaz123@gmail.com>
2023-08-11 17:45:52 -0500

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import RandomGreeting from './components/RandomGreeting';
 
 function App() {
   return (
-    <div className="App">
-      <RandomGreeting />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RandomGreeting />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
